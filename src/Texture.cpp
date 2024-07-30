d5a4c152222fde446e1f263a62aba4c955820c7b
clement-welsch <welsch.clement@gmail.com>
2024-07-30 17:52:54 +0200

    add Texture class

---
+++ src/Texture.cpp
@@ -0,0 +1,17 @@
+#include "Texture.h"
+
+Texture::Texture(const std::string& _path)
+{
+}
+
+Texture::~Texture()
+{
+}
+
+void Texture::Bind(unsigned int slot) const
+{
+}
+
+void Texture::Unbind() const
+{
+}
